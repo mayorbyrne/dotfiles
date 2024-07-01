84d6a77169a805bc549567ed1093ffccfc883fe8
Kevin Moritz <kevin@quavermusic.com>
2024-07-01 14:39:08 -0500

    gcc; copilot

--- lazyvim/.config/nvim/lua/plugins/example.lua
+++ lazyvim/.config/nvim/lua/plugins/example.lua
@@ -5,6 +5,10 @@ if true then
       "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
     },
     {
+      "github/copilot.vim",
+    },
+    { "numToStr/Comment.nvim", opts = {} },
+    {
       "nvim-neo-tree/neo-tree.nvim",
       opts = {
         window = {
