5ac84a91dedb07ede3891afa609fdbed63c53b4c
Kevin Moritz <kevin@quavermusic.com>
2024-07-03 08:23:10 -0500

    make fixes for vue / ts

--- lazyvim/.config/nvim/lua/plugins/example.lua
+++ lazyvim/.config/nvim/lua/plugins/example.lua
@@ -4,6 +4,9 @@ if true then
     {
       "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
     },
+    {
+      "github/copilot.vim",
+    },
     { "numToStr/Comment.nvim", opts = {} },
     {
       "nvim-neo-tree/neo-tree.nvim",
