22fab765ae71aabd855232bcb8f02c110d32948e
mayorbyrne <kmoritz@gmail.com>
2024-07-16 21:57:07 -0500

    dart format conform, lua

--- nvim_modular/.config/nvim/lua/plugins/conform.lua
+++ nvim_modular/.config/nvim/lua/plugins/conform.lua
@@ -1,37 +1,42 @@
 return {
-  -- {
-  -- 	"stevearc/conform.nvim",
-  -- 	config = function()
-  -- 		require("conform").setup({
-  -- 			formatters_by_ft = {
-  -- 				["markdown"] = { "prettier" },
-  -- 				["markdown.mdx"] = { "prettier" },
-  -- 				["typescript"] = { "prettier" },
-  -- 				["css"] = { "prettier" },
-  -- 				["graphql"] = { "prettier" },
-  -- 				["html"] = { "prettier" },
-  -- 				["javascript"] = { "prettier" },
-  -- 				["javascriptreact"] = { "prettier" },
-  -- 				["json"] = { "prettier" },
-  -- 				["less"] = { "prettier" },
-  -- 				["lua"] = { "stylua" },
-  -- 				["scss"] = { "prettier" },
-  -- 				["typescriptreact"] = { "prettier" },
-  -- 				["yaml"] = { "prettier" },
-  -- 			},
-  -- 			formatters = {
-  -- 				stylua = {
-  -- 					indent_type = "Spaces",
-  -- 					indent_width = 2,
-  -- 				},
-  -- 			},
-  -- 		})
-  --
-  -- 		local formatCode = function()
-  -- 			require("conform").format({ async = true, lsp_format = "fallback" })
-  -- 		end
-  --
-  -- 		vim.keymap.set("n", "<leader>cf", formatCode, { desc = "Format code" })
-  -- 	end,
-  -- },
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+
+        formatters_by_ft = {
+          ["markdown"] = { "prettier" },
+          ["markdown.mdx"] = { "prettier" },
+          ["typescript"] = { "prettier" },
+          ["css"] = { "prettier" },
+          ["graphql"] = { "prettier" },
+          ["html"] = { "prettier" },
+          ["javascript"] = { "prettier" },
+          ["javascriptreact"] = { "prettier" },
+          ["json"] = { "prettier" },
+          ["less"] = { "prettier" },
+          ["lua"] = { "stylua" },
+          ["scss"] = { "prettier" },
+          ["typescriptreact"] = { "prettier" },
+          ["yaml"] = { "prettier" },
+          ["dart"] = { "dart" },
+        },
+        formatters = {
+          stylua = {
+            args = { "--config-path", "~/.config/nvim/stylua.toml" },
+          },
+          dart = {
+            command = "dart",
+            args = { "format", "--line-length", "300" },
+          },
+        },
+      })
+
+      local formatCode = function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
+      end
+
+      vim.keymap.set("n", "<leader>cf", formatCode, { desc = "Format code" })
+    end,
+  },
 }
