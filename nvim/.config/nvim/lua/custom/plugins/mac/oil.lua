ef5c96a61b173c5f898372036e912e979280782c
mayorbyrne <kmoritz@gmail.com>
2024-09-25 15:35:56 -0500

    show hidden

--- nvim/.config/nvim/lua/custom/plugins/mac/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/mac/oil.lua
@@ -3,7 +3,11 @@ return {
     'stevearc/oil.nvim',
     opts = {},
     config = function()
-      require("oil").setup()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true,
+        }
+      })
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end
   },
