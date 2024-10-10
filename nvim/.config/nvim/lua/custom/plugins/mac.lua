d760122a42e99ca67361a9b6a304fe574df427b0
Kevin Moritz <kevin@quavermusic.com>
2024-10-10 10:52:36 -0500

    comment out delta paging

--- nvim/.config/nvim/lua/custom/plugins/mac.lua
+++ nvim/.config/nvim/lua/custom/plugins/mac.lua
@@ -27,11 +27,12 @@ require("custom.plugins.mac.nvim-tree")
 require("custom.plugins.mac.nvim-ts-autotag")
 require("custom.plugins.mac.drop")
 
-return { {
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {
-    },
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {},
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-} }
+  },
+}
