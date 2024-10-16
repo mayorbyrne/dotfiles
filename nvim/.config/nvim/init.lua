736d7e430a1d258bc20f17b95914dee9f3e23b9b
mayorbyrne <kmoritz@gmail.com>
2024-10-15 21:04:52 -0500

    add helpers for find/replace in current buffer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -200,6 +200,7 @@ require("lazy").setup({
         ["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
         ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
         ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+        ["<leader>fr"] = { name = "[F]ind and [R]eplace", _ = "which_key_ignore" },
         ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
         ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
         ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
