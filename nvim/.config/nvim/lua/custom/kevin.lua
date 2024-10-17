7abbdc5bf44faed81942caa11113cd923dfe74d1
mayorbyrne <kmoritz@gmail.com>
2024-10-16 23:32:08 -0500

    fix find/replace keymap

--- nvim/.config/nvim/lua/custom/kevin.lua
+++ nvim/.config/nvim/lua/custom/kevin.lua
@@ -1,4 +1,4 @@
-vim.g.netrw_sort_options = 'i'
+vim.g.netrw_sort_options = "i"
 vim.opt.termguicolors = true
 
 vim.g.minimap_width = 10
@@ -65,104 +65,93 @@ endif
 autocmd BufWritePre *.pl %s/\s\+$//e
 autocmd FileType dart,js,ts,html,css,md autocmd BufWritePre <buffer> %s/\s\+$//e
 
-]]);
+]])
 
 -- [[ Basic Keymaps ]]
-vim.keymap.set('n', 'd', '"_d', {})
-vim.keymap.set('v', 'd', '"_d', {})
-vim.keymap.set('n', 'c', '"_c', {})
-vim.keymap.set('v', 'c', '"_c', {})
-vim.keymap.set('n', 'diw', '"_diw', {})
-vim.keymap.set('v', 'diw', '"_diw', {})
-vim.keymap.set('n', 'ciw', '"_ciw', {})
-vim.keymap.set('v', 'ciw', '"_ciw', {})
+vim.keymap.set("n", "d", '"_d', {})
+vim.keymap.set("v", "d", '"_d', {})
+vim.keymap.set("n", "c", '"_c', {})
+vim.keymap.set("v", "c", '"_c', {})
+vim.keymap.set("n", "diw", '"_diw', {})
+vim.keymap.set("v", "diw", '"_diw', {})
+vim.keymap.set("n", "ciw", '"_ciw', {})
+vim.keymap.set("v", "ciw", '"_ciw', {})
 
 -- do not map c or d in SELECT mode
-vim.keymap.set('s', 'c', 'c', {})
-vim.keymap.set('s', 'd', 'd', {})
+vim.keymap.set("s", "c", "c", {})
+vim.keymap.set("s", "d", "d", {})
 
-vim.keymap.set('n', '<leader>bc', ':%bd|e#<CR>', { desc = "Close all buffers except current" })
+vim.keymap.set("n", "<leader>bc", ":%bd|e#<CR>", { desc = "Close all buffers except current" })
 
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-    expr = true,
-    replace_keycodes = false
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
 })
 vim.g.copilot_no_tab_map = true
 
-vim.keymap.set('n', '<leader>dd', ':Dashboard<CR>')
-vim.keymap.set('n', '<leader>dr', ':Telescope oldfiles<CR>')
-vim.keymap.set('n', '<leader>pp', ':Prettier<CR>')
+vim.keymap.set("n", "<leader>dd", ":Dashboard<CR>")
+vim.keymap.set("n", "<leader>dr", ":Telescope oldfiles<CR>")
+vim.keymap.set("n", "<leader>pp", ":Prettier<CR>")
 -- vim.keymap.set('n', '<leader>ee', ':Telescope file_browser<CR>')
 
-vim.keymap.set('n', '<leader>bn', ':bn<CR>')
-vim.keymap.set('n', '<leader>bp', ':bp<CR>')
-vim.keymap.set('t', '<leader>bd', ':bd!<CR>')
-vim.keymap.set('n', '<leader>bd', function()
-    if vim.bo.buftype == 'terminal' then
-        vim.cmd 'bd!'
-    else
-        vim.cmd 'bd'
-    end
+vim.keymap.set("n", "<leader>bn", ":bn<CR>")
+vim.keymap.set("n", "<leader>bp", ":bp<CR>")
+vim.keymap.set("t", "<leader>bd", ":bd!<CR>")
+vim.keymap.set("n", "<leader>bd", function()
+  if vim.bo.buftype == "terminal" then
+    vim.cmd("bd!")
+  else
+    vim.cmd("bd")
+  end
 end)
 
-vim.cmd('ca wq bd!')
-vim.cmd('ca q bd!')
+vim.cmd("ca wq bd!")
+vim.cmd("ca q bd!")
 
-vim.keymap.set('n', '<leader>kb', ':VimuxRunCommand "webdev serve"<CR>')
-vim.keymap.set('n', '<leader>kg', ':VimuxRunCommand "lazygit"<CR>')
-vim.keymap.set('n', '<leader>gi', ':Git<CR>', { desc = "Git Fugitive" })
+vim.keymap.set("n", "<leader>kb", ':VimuxRunCommand "webdev serve"<CR>')
+vim.keymap.set("n", "<leader>kg", ':VimuxRunCommand "lazygit"<CR>')
+vim.keymap.set("n", "<leader>gi", ":Git<CR>", { desc = "Git Fugitive" })
 
-vim.keymap.set('n', '<leader>e', ':lua vim.diagnostic.open_float(0, {scope="line"})<CR>', { desc = "Line Diagnostics" })
+vim.keymap.set("n", "<leader>e", ':lua vim.diagnostic.open_float(0, {scope="line"})<CR>', { desc = "Line Diagnostics" })
 
-vim.keymap.set("n", "<leader>xx", ':Trouble diagnostics<CR>', { desc = "Trouble Open" })
-vim.keymap.set("n", "<leader>xc", ':Trouble close<CR>', { desc = "Trouble Close" })
-vim.keymap.set("n", "<leader>xp", ':Trouble prev<CR>', { desc = "Trouble Previous" })
-vim.keymap.set("n", "<leader>xn", ':Trouble next<CR>', { desc = "Trouble Next" })
+vim.keymap.set("n", "<leader>xx", ":Trouble diagnostics<CR>", { desc = "Trouble Open" })
+vim.keymap.set("n", "<leader>xc", ":Trouble close<CR>", { desc = "Trouble Close" })
+vim.keymap.set("n", "<leader>xp", ":Trouble prev<CR>", { desc = "Trouble Previous" })
+vim.keymap.set("n", "<leader>xn", ":Trouble next<CR>", { desc = "Trouble Next" })
 
-vim.keymap.set('n', '<leader>qq', ':copen<CR>', { desc = "Quickfix Open" })
-vim.keymap.set('n', '<leader>qo', ':copen<CR>', { desc = "Quickfix Open" })
-vim.keymap.set('n', '<leader>qc', ':cclose<CR>', { desc = "Quickfix Close" })
-vim.keymap.set('n', '<leader>qn', ':cnext<CR>', { desc = "Quickfix Next" })
-vim.keymap.set('n', '<leader>qp', ':cprev<CR>', { desc = "Quickfix Previous" })
-vim.keymap.set('n', '<leader>qf', ':cfirst<CR>', { desc = "Quickfix First" })
-vim.keymap.set('n', '<leader>ql', ':clast<CR>', { desc = "Quickfix Last" })
+vim.keymap.set("n", "<leader>qq", ":copen<CR>", { desc = "Quickfix Open" })
+vim.keymap.set("n", "<leader>qo", ":copen<CR>", { desc = "Quickfix Open" })
+vim.keymap.set("n", "<leader>qc", ":cclose<CR>", { desc = "Quickfix Close" })
+vim.keymap.set("n", "<leader>qn", ":cnext<CR>", { desc = "Quickfix Next" })
+vim.keymap.set("n", "<leader>qp", ":cprev<CR>", { desc = "Quickfix Previous" })
+vim.keymap.set("n", "<leader>qf", ":cfirst<CR>", { desc = "Quickfix First" })
+vim.keymap.set("n", "<leader>ql", ":clast<CR>", { desc = "Quickfix Last" })
 
-vim.keymap.set('n', '<leader>lr', ':LspRestart<CR>', { desc = "LSP Restart" })
-vim.keymap.set('n', '<leader>li', ':LspInfo<CR>', { desc = "LSP Info" })
+vim.keymap.set("n", "<leader>lr", ":LspRestart<CR>", { desc = "LSP Restart" })
+vim.keymap.set("n", "<leader>li", ":LspInfo<CR>", { desc = "LSP Info" })
 
-vim.keymap.set('n', '<leader>to', ':NvimTreeOpen<CR>', { desc = "NvimTree Open" })
-vim.keymap.set('n', '<leader>tc', ':NvimTreeClose<CR>', { desc = "NvimTree Close" })
-vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', { desc = "NvimTree Toggle" })
-vim.keymap.set('n', '<leader>tf', ':NvimTreeFocus<CR>', { desc = "NvimTree Focus" })
+vim.keymap.set("n", "<leader>to", ":NvimTreeOpen<CR>", { desc = "NvimTree Open" })
+vim.keymap.set("n", "<leader>tc", ":NvimTreeClose<CR>", { desc = "NvimTree Close" })
+vim.keymap.set("n", "<leader>tt", ":NvimTreeToggle<CR>", { desc = "NvimTree Toggle" })
+vim.keymap.set("n", "<leader>tf", ":NvimTreeFocus<CR>", { desc = "NvimTree Focus" })
 
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
 
 vim.keymap.set("n", "<leader>m", ":RenderMarkdown toggle<CR>", { desc = "Markdown Preview" })
 
-vim.keymap.set("n", "<leader>frc", "yiw :%s/<C-R>\"//gc<LEFT><LEFT><LEFT>", { desc = "[C]onfirm" })
-vim.keymap.set("n", "<leader>fra", "yiw :%s/<C-R>\"//g<LEFT><LEFT>", { desc = "[A]ll" })
-
-local function yank(isConfirm)
+vim.keymap.set("n", "<leader>frya", function()
   local char = vim.fn.input("Character to yank inside: ")
-  vim.cmd("normal! yi" .. char)
-
-  if isConfirm then
-    vim.fn.feedkeys(":%s/<C-R>\"//gc", "n")
-    vim.cmd("normal! <LEFT><LEFT><LEFT>")
-    end
-
-  vim.fn.feedkeys(":%s/<C-R>\"//g", "n")
-  vim.cmd("normal! <LEFT><LEFT>")
-end
-vim.keymap.set("n", "<leader>frya", yank, { desc = "Choose [Y]ank Character [A]ll" })
-vim.keymap.set("n", "<leader>fryc", yank, { desc = "Choose [Y]ank Character [C]onfirm" })
-
-vim.keymap.set("n", "<leader>fr\'", "yi\' :%s/<C-R>\"//gc<LEFT><LEFT><LEFT>", { desc = "Inside [\']"})
+  return "yi" .. char .. ' :%s/<C-R>"//g<LEFT><LEFT>'
+end, { desc = "Choose [Y]ank Character [A]ll", expr = true })
+vim.keymap.set("n", "<leader>fryc", function()
+  local char = vim.fn.input("Character to yank inside: ")
+  return "yi" .. char .. ' :%s/<C-R>"//gc<LEFT><LEFT><LEFT>'
+end, { desc = "Choose [Y]ank Character [C]onfirm", expr = true })
 
 local editCfg = require("custom.custom")
 
 return {
-    editCfg = editCfg,
-    editKevin = editCfg.editKevin,
-    editPlugins = editCfg.editPlugins,
+  editCfg = editCfg,
+  editKevin = editCfg.editKevin,
+  editPlugins = editCfg.editPlugins,
 }
