49c87b613a9f23b70a88ba09ebe8ee80017e68b0
Kevin Moritz <kevin@quavermusic.com>
2024-10-28 09:39:32 -0500

    fix prettierd on vue files

--- nvim/.config/nvim/lua/custom/plugins/mac/prettier.lua
+++ nvim/.config/nvim/lua/custom/plugins/mac/prettier.lua
@@ -2,7 +2,8 @@ return {
   {
     "MunifTanjim/prettier.nvim",
     config = function()
-      require("prettier").setup({
+      local prettier = require("prettier")
+      prettier.setup({
         bin = "prettierd", -- or `'prettierd'` (v0.23.3+)
         filetypes = {
           "css",
@@ -16,7 +17,21 @@ return {
           "scss",
           "typescript",
           "typescriptreact",
+          "vue",
           "yaml",
+        },
+        ["null-ls"] = {
+          condition = function()
+            return prettier.config_exists({
+              -- if `false`, skips checking `package.json` for `"prettier"` key
+              check_package_json = true,
+            })
+          end,
+          runtime_condition = function(params)
+            -- return false to skip running prettier
+            return true
+          end,
+          timeout = 5000,
         },
       })
     end,
