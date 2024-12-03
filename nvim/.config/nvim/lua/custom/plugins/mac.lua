1002bdaab2c1dce9cb15bd91dafbdfdffda1f640
Kevin Moritz <kevin@quavermusic.com>
2024-12-03 07:36:51 -0600

    feat: update plugin versions and add snippet converter
    - Updated various plugin versions in lazy-lock.json to their latest
    commits.
    - Added new plugin "snippet-converter.nvim" to mac.lua.
    - Modified copilot.lua to include build step for CopilotChat.nvim.

--- nvim/.config/nvim/lua/custom/plugins/mac.lua
+++ nvim/.config/nvim/lua/custom/plugins/mac.lua
@@ -28,5 +28,6 @@ require("custom.plugins.mac.nvim-ts-autotag")
 require("custom.plugins.mac.drop")
 require("custom.plugins.mac.render-markdown")
 require("custom.plugins.mac.workspace-diagnostics")
+require("custom.plugins.mac.snippet-converter")
 
 return {}
--- nvim/.config/nvim/lua/custom/plugins/mac/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/mac/copilot.lua
@@ -2,13 +2,13 @@ return {
   { "github/copilot.vim" },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
-      { "github/copilot.vim" },
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
+    build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
+      debug = true, -- Enable debug mode
     },
   },
 }
