a6856df16be6137c7916c75c0ab6434e06c3b069
mayorbyrne <kmoritz@gmail.com>
2025-02-03 21:20:25 -0600

    fix copilotchat.nvim on windows

--- nvim/.config/nvim/lua/custom/plugins/mac/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/mac/copilot.lua
@@ -6,7 +6,7 @@ return {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
-    build = "make tiktoken",    -- Only on MacOS or Linux
+    -- build = "make tiktoken",    -- Only on MacOS or Linux
     opts = {
       debug = true,             -- Enable debug mode
       prompts = {
