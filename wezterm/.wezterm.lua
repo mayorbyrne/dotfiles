d167f3b9a8b59d20dc64ff09f37862d1c9f04d53
Kevin Moritz <mayorbyrne@users.noreply.github.com>
2025-02-11 07:31:34 -0600

    Update .wezterm.lua for windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,11 +4,31 @@ local mux = wezterm.mux
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.wsl_domains = {
+  {
+    -- The name of this specific domain.  Must be unique amonst all types
+    -- of domain in the configuration file.
+    name = 'WSL:Ubuntu',
+
+    -- The name of the distribution.  This identifies the WSL distribution.
+    -- It must match a valid distribution from your `wsl -l -v` output in
+    -- order for the domain to be useful.
+    distribution = 'Ubuntu',
+  },
+}
+config.default_domain = 'WSL:Ubuntu'
+
 config.check_for_updates = true
 
 -- This is where you actually apply your config choices
 
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 -- Use the defaults as a base
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
@@ -22,15 +42,17 @@ config.colors = {
 }
 
 config.font = wezterm.font("Fira Code", { weight = "DemiBold" })
-config.font_size = 12
+config.font_size = 11
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.window_frame = {
-  border_bottom_height = "0.1cell",
+  -- border_bottom_height = "0.6cell",
   border_bottom_color = "#123456",
 }
 
 config.audible_bell = "Disabled"
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE";
 
 -- and finally, return the configuration to wezterm
 wezterm.on("gui-startup", function(cmd)
@@ -47,7 +69,8 @@ wezterm.on("gui-startup", function(cmd)
 
   -- You probably don't need both, but you can also set the positions after spawn.
   window:gui_window():set_position(active.x, active.y)
-  window:gui_window():set_inner_size(active.width, active.height)
+  window:gui_window():maximize()
+  -- window:gui_window():set_inner_size(active.width, active.height - 20)
 end)
 
 config.default_cursor_style = "BlinkingBlock"
