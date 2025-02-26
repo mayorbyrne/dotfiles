f3ad1b34ba5c36d16e95014c05273b162a42b3bd
Kevin Moritz <kevin@quavered.com>
2025-02-26 08:35:30 -0600

    override ctrl+shift commands for the love of god

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,16 +14,16 @@ config.wsl_domains = {
   {
     -- The name of this specific domain.  Must be unique amonst all types
     -- of domain in the configuration file.
-    name = 'WSL:Ubuntu',
+    name = "WSL:Ubuntu",
 
     -- The name of the distribution.  This identifies the WSL distribution.
     -- It must match a valid distribution from your `wsl -l -v` output in
     -- order for the domain to be useful.
-    distribution = 'Ubuntu',
-    default_cwd = '~',
+    distribution = "Ubuntu",
+    default_cwd = "~",
   },
 }
-config.default_domain = 'WSL:Ubuntu'
+config.default_domain = "WSL:Ubuntu"
 
 config.check_for_updates = true
 
@@ -54,7 +54,7 @@ config.window_frame = {
 
 config.audible_bell = "Disabled"
 
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE";
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 -- and finally, return the configuration to wezterm
 wezterm.on("gui-startup", function(cmd)
@@ -85,5 +85,23 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 else
   config.default_prog = wezterm.Default_prog
 end
+
+config.keys = {
+  {
+    key = "v",
+    mods = "CTRL",
+    action = wezterm.action.PasteFrom("Clipboard"),
+  },
+  {
+    key = "t",
+    mods = "CTRL",
+    action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+  },
+  {
+    key = "w",
+    mods = "CTRL",
+    action = wezterm.action.CloseCurrentTab({ confirm = true }),
+  },
+}
 
 return config
