a041d7ce8dd44ca4089a7d8450135ae5165e56b1
Kevin Moritz <mayorbyrne@users.noreply.github.com>
2025-04-30 21:57:47 -0500

    Update setup.sh

--- install_scripts/linux/setup.sh
+++ install_scripts/linux/setup.sh
@@ -15,7 +15,31 @@ command -v zsh | sudo tee -a /etc/shells
 echo "Making zsh default shell..."
 sudo chsh -s $(which zsh) $USER
 
-echo "SWITCH TO ZSH NOW and continue"
+echo "Installing wezterm..."
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo apt update
+sudo apt install wezterm
+
+echo "Installing stow..."
+brew install stow
+
+echo "Cloning dotfiles..."
+git clone https://www.github.com/mayorbyrne/dotfiles.git ~/.dotfiles
+
+echo "Stowing dotfiles..."
+cd ~/.dotfiles
+stow --adopt -v base
+stow --adopt -v lazygit
+stow --adopt -v nvim
+stow --adopt -v scripts
+stow --adopt -v tmux
+stow --adopt -v wezterm
+stow --adopt -v yazi
+git restore .
+source ~/.zshrc
+
+echo "SWITCH TO wezterm now and continue"
 return 1
 
 echo "Install curl..."
@@ -86,24 +110,6 @@ sudo apt install build-essential
 
 echo "firacode"
 sudo apt install fonts-firacode
-
-echo "Installing stow..."
-brew install stow
-
-echo "Cloning dotfiles..."
-git clone https://www.github.com/mayorbyrne/dotfiles.git ~/.dotfiles
-
-echo "Stowing dotfiles..."
-cd ~/.dotfiles
-stow --adopt -v base
-stow --adopt -v lazygit
-stow --adopt -v nvim
-stow --adopt -v scripts
-stow --adopt -v tmux
-stow --adopt -v wezterm
-stow --adopt -v yazi
-git restore .
-source ~/.zshrc
 
 cd ~
 wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.935/gcm-linux_amd64.2.0.935.deb
